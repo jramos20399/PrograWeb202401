a5384d4fef45ba9d3fa5dd9829958111c9b56961
juanpabloramosp <juanpabloramosp@hotmail.com>
2024-03-12 19:30:01 -0600

    Register Completo

---
+++ Northwind/BackEnd/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using BackEnd.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+                this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        [Route("registrar")]
+        public async Task<IActionResult> Registrar([FromBody]RegisterModel model)
+        {
+            var userExist = await userManager.FindByNameAsync(model.Username);
+            if (userExist != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+
+
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        }
+
+
+    }
+}
